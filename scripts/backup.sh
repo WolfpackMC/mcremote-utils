3db787ca2796c7d6e0e2d378f8866714fe5738d5
Kalka <k@kalka.io>
2023-01-05 09:32:34 -0500

    actually fix URL

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,2 @@
 
-deno run -A https://github.com/WolfpackMC/mcremote-utils/raw/main/src/deno/s3_backup.ts -n $1 -d $2
+deno run -A https://raw.githubusercontent.com/WolfpackMC/mcremote-utils/master/src/deno/s3_backup.ts -n $1 -d $2
