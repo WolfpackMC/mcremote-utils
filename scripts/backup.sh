01707c54944e2009c2479b174eb6c64b2152991b
Kalka <k@kalka.io>
2023-01-05 09:31:47 -0500

    change URL

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,2 @@
 
-deno run -A https://github.com/kalkafox/mcremote-utils/raw/main/src/deno/s3_backup.ts -n $1 -d $2
+deno run -A https://github.com/WolfpackMC/mcremote-utils/raw/main/src/deno/s3_backup.ts -n $1 -d $2
